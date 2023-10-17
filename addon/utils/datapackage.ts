fc3390051c9b75ff8060666edb57a25219336e13
James Wilmot <jameswilmot2000@gmail.com>
2023-10-17 12:40:15 +1100

    Generated datapackage util

---
+++ addon/utils/datapackage.ts
@@ -0,0 +1,3 @@
+export default function datapackage() {
+  return true;
+}
---
+++ tests/unit/utils/datapackage-test.ts
@@ -0,0 +1,10 @@
+import datapackage from 'dummy/utils/datapackage';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | datapackage', function () {
+  // TODO: Replace this with your real tests.
+  test('it works', function (assert) {
+    const result = datapackage();
+    assert.ok(result);
+  });
+});
